d57e450456b82623235a6562487d92308a4a2945
Shashwat <shashwatpanda4@gmail.com>
2023-03-21 12:10:15 +0530

    built banner component

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -2,8 +2,7 @@ export default function HomePage() {
     return (
        
         <div>
-            <h1 className="text-4xl">welcome to the blog</h1>
-            <p>Some content</p>
+           
         </div>
     )
 }
