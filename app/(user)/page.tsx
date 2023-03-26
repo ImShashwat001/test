d9f53187c88411d45b9d6d3c073bbc60b63884c7
Shashwat <shashwatpanda4@gmail.com>
2023-03-26 19:29:48 +0530

    live links set

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,8 +1,12 @@
+import { previewData } from "next/headers";
+
 export default function HomePage() {
+    if (previewData()) {
+        return <div>Preview Mode</div>;
+    }
     return (
-       
         <div>
-           
+           <h1>Not in Preview mode</h1>
         </div>
     )
 }
