0aecfa01b82bc5bfc8535ef001f0d5790050be97
Shashwat <shashwatpanda4@gmail.com>
2023-03-26 22:20:39 +0530

    setting up preview suspense

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,9 +1,22 @@
 import { previewData } from "next/headers";
+import { groq } from "next-sanity";
+import { client } from "@/lib/sanity.client";
 
-export default function HomePage() {
+const query = groq`
+    *[_type=='post'] {
+        ...,
+        author->,
+        categories[]->
+    } | order(_createdAt desc)
+`
+
+export default async function HomePage() {
     if (previewData()) {
         return <div>Preview Mode</div>;
     }
+
+    const posts = await client.fetch(query);
+
     return (
         <div>
            <h1>Not in Preview mode</h1>
