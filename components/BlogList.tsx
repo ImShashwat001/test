ed6c67e6004b00fd7c0ef44420f50b932be34be4
Shashwat <shashwatpanda4@gmail.com>
2023-04-24 21:29:04 +0530

    image size assigned with tailwindcss

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -18,15 +18,31 @@ function BlogList({posts}: Props) {
 
         {posts.map((post) => (
           <div key={post._id} className="flex flex-col group cursor-pointer">
-            <div>
+            <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
             <Image 
               className="object-cover object-left lg:object-center"
               src={urlFor(post.mainImage).url()}
               alt={post.author.name}
               fill   
             />
+            <div>
+            <div>
+              <p className="font-bold">{post.title}</p>
+              <p>
+
+                {new Date(post._createdAt).toLocaleDateString("en-US", {
+                  day: "numeric",
+                  month: "long",
+                  year: "numeric",
+                })}
+              
+              </p>
+            </div>
+            </div>
+            
           </div>
           </div>
+          
           
         ))}
         
